0ced625a96eed515939667791f7bd4270863fd87
xfl12345 <xfl1046539849@qq.com>
2019-01-03 14:36:47 +0800

    Update warnUserInput.js
    针对表格排版，给提示语元素进行精确定位。

--- warnUserInput.js
+++ warnUserInput.js
@@ -1,4 +1,4 @@
-﻿var warmingDivCount;
+var warmingDivCount;
 var xzzsInit=function(limitTarget){
 	for(var i=0,tmpvar; i<limitTarget.length ;i++){
 		//var maxLen=limitTarget[i].getAttribute("maxlength");
@@ -8,44 +8,87 @@ var xzzsInit=function(limitTarget){
 			limitTarget[i].setAttribute("id","inputNo_"+tmpvar);
 			EleId="inputNo_"+tmpvar;
 		}
-		//var inputE=document.getElementById(EleId);
-		var parentOfinputE=limitTarget[i].parentNode;
+		//alert(limitTarget[i]);
+		var parentOfinputE=xzzsParentNode(limitTarget[i]);
+		
 		if(!parentOfinputE.getAttribute("id"))
 			parentOfinputE.setAttribute("id",EleId+"_"+tmpvar);
 		if(  !(limitTarget[i].getAttribute("type")=="text" || limitTarget[i].getAttribute("type")=="password")  )
 		continue;
-		limitTarget[i].setAttribute("onfocus","xzzsCreate('"+EleId+"')");
-		limitTarget[i].setAttribute("onblur","xzzsRemove('"+EleId+"')");
-		limitTarget[i].setAttribute("onkeyup","xzzs('"+EleId+"')");
+		/*
+		limitTarget[i].addEventListener("focus",function(){xzzsCreate(EleId);});
+		limitTarget[i].addEventListener("blur",function(){xzzsRemove(EleId);});
+		limitTarget[i].addEventListener("keyup",function(){xzzs(EleId);});
+		*/
+		limitTarget[i].setAttribute("onfocus","xzzsCreate('"+EleId+"');");
+		limitTarget[i].setAttribute("onblur","xzzsRemove('"+EleId+"');");
+		limitTarget[i].setAttribute("onkeyup","xzzs('"+EleId+"');");
 	}
 }
 var xzzsCreate=function(inputE){
-	var aim = document.getElementById(inputE).parentNode;
+	var aim = xzzsParentNode(document.getElementById(inputE));
 	var idOfMarkedWords=inputE+'xz';
+	var markedWords;
 	if(document.getElementById(idOfMarkedWords))
 		return;
-	var markedWords = document.createElement("div");
-	markedWords.setAttribute("id",idOfMarkedWords);
-	markedWords.setAttribute("class","JSinsert");
-	markedWords.setAttribute("style","display: block;");
+	if(aim.tagName=="TR" || aim.tagName=="tr"){
+		markedWords = document.createElement("td");
+		markedWords.setAttribute("id",idOfMarkedWords);
+		markedWords.setAttribute("class","JSinsert");
+		markedWords.setAttribute("style","display: block;");
+		var tmpTr=document.createElement("tr");
+		tmpTr.setAttribute("id",idOfMarkedWords+"Tr");
+		autoSetTdLocation(aim,tmpTr);
+		tmpTr.appendChild(markedWords);
+		insertAfterNode(tmpTr,aim);
+	}
+	else{
+		markedWords = document.createElement("div");
+		markedWords.setAttribute("id",idOfMarkedWords);
+		markedWords.setAttribute("class","JSinsert");
+		markedWords.setAttribute("style","display: block;");
+		aim.appendChild(markedWords);
+	}
 	//markedWords.innerHTML="<br>Worked!";
-	aim.appendChild(markedWords);
 	warmingDivCount++;
 }
 var xzzsRemove=function(inputE){
+	var idOfMarkedWords=inputE+'xz';
 	/* DIY code start*/
 	if(friendlyWarming(inputE)){
-		var idOfMarkedWords=inputE+'xz';
+		
 		var markedWords = document.getElementById(idOfMarkedWords);
 		markedWords.style.display="block";
 		return;
 	}
 	/* DIY code end*/
-	var parentOfthisnode = document.getElementById(inputE).parentNode;
-	var idOfMarkedWords=inputE+'xz';
+	var parentOfthisnode = xzzsParentNode(document.getElementById(inputE));
 	var markedWords = document.getElementById(idOfMarkedWords);
-	parentOfthisnode.removeChild(markedWords);
+	if(parentOfthisnode.tagName=="TR" || parentOfthisnode.tagName=="tr"){
+		var tmpTr=document.getElementById(idOfMarkedWords+"Tr");
+		//alert(parentOfthisnode.parentNode.tagName);
+		parentOfthisnode.parentNode.removeChild(tmpTr);
+	}
+	else{
+		parentOfthisnode.removeChild(markedWords);
+	}
 	warmingDivCount--;
+};
+var xzzsParentNode=function(inputE){
+	var child=inputE;
+	var parentEle=child.parentNode;
+	if(parentEle.tagName=="TD")
+		for(parentEle=parentEle.parentNode ; parentEle.tagName != "TR" ; parentEle=parentEle.parentNode );
+	if(parentEle.tagName=="td")
+		for(parentEle=parentEle.parentNode ; parentEle.tagName != "tr" ; parentEle=parentEle.parentNode );
+	/*
+	var tmpvar=document.getElementById("jsDebug");
+	tmpvar.innerHTML=tmpvar.innerHTML+child.tagName+"&nbsp;"+parentEle;
+	if(parentEle)
+		tmpvar.innerHTML=tmpvar.innerHTML+"&nbsp;"+parentEle.tagName;
+	tmpvar.innerHTML=tmpvar.innerHTML+"<br>";
+	*/
+	return parentEle;
 };
 var xzzs=function(inputE){
 	//var inputE1 =inputE.toString();
@@ -131,11 +174,28 @@ function inputLenghtCheck(Target){
 		if(Target.value==null){
 			return false;
 		}else{
-			if(Target.value.length<=1)
+			if(Target.value.length<=1){
 				return false;
+			}
 		}
 	}
 	return true;
+};
+function insertAfterNode(newElement, targentElement) {
+    var parent = targentElement.parentNode;
+    if (parent.lastChild == targentElement) {
+        parent.appendChild(newElement);
+    } else {
+        parent.insertBefore(newElement, targentElement.nextSibling)
+    }
+};
+function autoSetTdLocation(trOfInputE,aim){
+	var tdEle=trOfInputE.getElementsByTagName("td");
+	//alert("共找到 "+tdEle.length+" 个td块");
+	for(var i=0;i<tdEle.length-1;i++){
+		var tmpTd=document.createElement("td");
+		aim.appendChild(tmpTd);
+	}
 };
 var finalcheck=function(){
 	
@@ -154,8 +214,14 @@ var finalcheck=function(){
     	alert("还有些东东没搞定呢！");
     return is_done;
 };
-
-window.onload=function(){	
+var outputDebug=function(){
+	
+};
+window.onload=function(){
+	var kuan=document.documentElement.clientWidth;
+	var gao=document.documentElement.clientHeight;
+	document.styleSheets[0].cssRules[0].style.left=kuan/2 +"px";
+	
 	var AllinputE = document.getElementsByTagName("input");
 	xzzsInit(AllinputE);
 	var AlltextareaE = document.getElementsByTagName("textarea");
